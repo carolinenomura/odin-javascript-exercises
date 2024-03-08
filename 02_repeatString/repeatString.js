cc2b977b517a975c2182b9ea8afc48f2c972f8bc
yumi <carolineyumin@gmail.com>
2024-03-08 11:53:08 -0300

    repeatString.js changed and passes all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (str, amount) {
+  let result = "";
+  if (Math.sign(amount) === -1) return "ERROR";
+  for (let i = 0; i < amount; i++) {
+    result += str;
+  }
+  return result;
 };
 
 // Do not edit below this line
