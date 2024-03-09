c19810f55c622e1063a0ab48b3c99d550325b30b
Caroline Yumi <carolineyumin@gmail.com>
2024-03-09 20:20:14 -0300

    sumAll.js changed and passes all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (value1, value2) {
+  // Checks if any of the values is not a number before they are converted with Math methods
+  if (typeof value1 !== "number" || typeof value2 !== "number") {
+    return "ERROR";
+  }
+  const first = Math.min(value1, value2);
+  const last = Math.max(value1, value2);
 
+  // Checks if any of the values is negative
+  if (Math.sign(first) === -1 || Math.sign(last) === -1) {
+    return "ERROR";
+  }
+
+  let sum = 0;
+  for (let i = first; i <= last; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
