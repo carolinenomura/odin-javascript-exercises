fae7672c879b5630786909102210f8fdc161466f
Caroline Yumi <carolineyumin@gmail.com>
2024-03-09 13:04:05 -0300

    removeFromArray.js changed and passes all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr) {
+  const args = Array.prototype.slice.call(arguments, 1);
+  // Keeps only elements not passed in the arguments
+  return arr.filter((element) => !args.includes(element));
 };
 
 // Do not edit below this line
