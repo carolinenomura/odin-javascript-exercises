83b2d0407591440bb772235808a4b21b6cfa7f11
yumi <carolineyumin@gmail.com>
2024-03-09 01:10:57 -0300

    reverseString.js changed and passes all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let strArray = str.split("");
+  strArray.reverse();
+  let reversedStr = strArray.join("");
+  return reversedStr;
 };
 
 // Do not edit below this line
