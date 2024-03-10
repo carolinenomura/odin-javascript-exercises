db66cb00b3c99a4745ff606a7e8c38fd88822f54
Caroline Yumi <carolineyumin@gmail.com>
2024-03-10 15:13:57 -0300

    findTheOldest.js changed and passes all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let oldestAge = 0;
+  let oldestPerson;
+  let age;
 
+  people.forEach((person) => {
+    if (person.yearOfDeath == undefined) {
+      const currentYear = new Date().getFullYear();
+      age = currentYear - person.yearOfBirth;
+    } else {
+      age = person.yearOfDeath - person.yearOfBirth;
+    }
+    if (age > oldestAge) {
+      oldestAge = age;
+      oldestPerson = person;
+    }
+  });
+  return oldestPerson;
 };
 
 // Do not edit below this line
