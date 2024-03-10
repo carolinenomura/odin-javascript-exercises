028277486fc2974ff55510b1fb75dd799e6316a1
Caroline Yumi <carolineyumin@gmail.com>
2024-03-10 13:57:34 -0300

    fibonacci.js changed and passes all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (member) {
+  if (member == 0) {
+    return 0;
+  } else if (member < 0) {
+    return 'OOPS';
+  } else {
+    let f0 = 0;
+    let f1 = 1;
+    let f2 = f1;
+    for (let i = 1; i < member; i++) {
+      f2 = f0 + f1;
+      f0 = f1;
+      f1 = f2;
+    }
+    return f2;
+  }
 };
 
 // Do not edit below this line
