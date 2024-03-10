11c398b78203b3ec23aa379afe94c1fcdb1d1a12
Caroline Yumi <carolineyumin@gmail.com>
2024-03-09 21:10:13 -0300

    tempConversion.js changed and passes all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (valueF) {
+  let valueC = (valueF - 32) / 1.8;
+  return Math.round(valueC * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (valueC) {
+  let valueF = valueC * 1.8 + 32;
+  return Math.round(valueF * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
