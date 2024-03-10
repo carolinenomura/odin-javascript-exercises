11e63e0e6e2a7365e60d61af2774be5144bfd367
Caroline Yumi <carolineyumin@gmail.com>
2024-03-09 23:38:37 -0300

    palindromes.js changed and passes all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const strippedStr = str
+    .toLowerCase()
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g, '')
+    .split('');
+  const forwardStr = strippedStr.join('');
+  const backwardStr = strippedStr.reverse().join('');
+  return forwardStr === backwardStr;
 };
 
 // Do not edit below this line
